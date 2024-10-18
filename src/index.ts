66f20b24a28dfd3c7766f77357f06c6d66f70cda
shlroland <shlroland1995@gmail.com>
2024-10-18 11:56:13 +0800

    🏗️ Perfect vite package build

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { default as czGit } from './cz-git-prompts'
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from "vite";
 import virtualGitmojiJson from "./configs/virtual-gitmoji-json";
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [virtualGitmojiJson()],
+  plugins: [virtualGitmojiJson(), dts({
+    entryRoot: 'src',
+    rollupTypes: true,
+  }),],
   build: {
     lib: {
-      entry: ['src/cz-git-prompts/index.ts'],
+      entry: {
+        index: 'src/index.ts',
+        'cz-git': 'src/cz-git-prompts/index.ts',
+      },
       formats: ['es', 'cjs'],
-    }
-  }
+
+    },
+    emptyOutDir: true,
+  },
 })
